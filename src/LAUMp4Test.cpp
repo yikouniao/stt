eddfd8aa28ea239a3f03e027949cbc8879f0ae6d
yikouniao <chinoandtippi@gmail.com>
2016-10-20 10:09:50 +0800

    add files

---
+++ src/LAUMp4Test.cpp
@@ -0,0 +1,122 @@
+// LAUMp4Test.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "LAUMp4Test.h"
+#include "LAUMp4TestDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CLAUMp4TestApp
+
+BEGIN_MESSAGE_MAP(CLAUMp4TestApp, CWinApp)
+	//{{AFX_MSG_MAP(CLAUMp4TestApp)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+		//    DO NOT EDIT what you see in these blocks of generated code!
+	//}}AFX_MSG
+	ON_COMMAND(ID_HELP, CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CLAUMp4TestApp construction
+
+CLAUMp4TestApp::CLAUMp4TestApp()
+{
+	// TODO: add construction code here,
+	// Place all significant initialization in InitInstance
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// The one and only CLAUMp4TestApp object
+
+CLAUMp4TestApp theApp;
+
+/////////////////////////////////////////////////////////////////////////////
+// CLAUMp4TestApp initialization
+
+BOOL CLAUMp4TestApp::InitInstance()
+{
+	m_hLonDll = NULL;
+	char currdir[MAX_PATH];
+	GetModuleFileName(NULL, currdir, MAX_PATH);
+	CString str;
+	str.Format("%s", currdir);
+	str = str.Left(str.ReverseFind(_T('\\')));
+	sprintf(currdir, "%s", str);
+
+
+	//::OleInitialize(NULL);
+	::AfxEnableControlContainer();
+
+	// Standard initialization
+	// If you are not using these features and wish to reduce the size
+	//  of your final executable, you should remove from the following
+	//  the specific initialization routines you do not need.
+
+#ifdef _AFXDLL
+	Enable3dControls();			// Call this when using MFC in a shared DLL
+#else
+	Enable3dControlsStatic();	// Call this when linking to MFC statically
+#endif
+
+
+	char lang[10];
+	char langinifile[256];
+	sprintf(langinifile,"%s\\lang.ini",currdir);
+	GetPrivateProfileString("LANGUAGE","showLang","chinese",lang,10,langinifile);
+//	GetPrivateProfileString("LANGUAGE","Lang","chinese",lang,10,"\\lang.ini");
+	if(strcmp(lang,"english") == 0)
+	{
+		m_hLonDll = LoadLibrary("en_resource.dll");
+		if (m_hLonDll != NULL)
+		{
+			::AfxSetResourceHandle(m_hLonDll);
+		}
+	}
+	else
+	{
+		//默认中文资源
+	}
+
+    /* NetDllHandle = LoadLibrary("C:/WINNT/system32/NetClient.dll");
+	if(NetDllHandle == NULL)
+	{
+		TRACE("Load Library error %d\n",GetLastError());
+	}*/
+	//AFX_MANAGE_STATE(AfxGetStaticModuleState());
+	CLAUMp4TestDlg dlg;
+	m_pMainWnd = &dlg;
+	int nResponse = dlg.DoModal();
+	if (nResponse == IDOK)
+	{
+
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with OK
+	}
+	else if (nResponse == IDCANCEL)
+	{
+		// TODO: Place code here to handle when the dialog is
+		//  dismissed with Cancel
+	}
+
+	// Since the dialog has been closed, return FALSE so that we exit the
+	//  application, rather than start the application's message pump.
+
+	return FALSE;
+}
+
+int CLAUMp4TestApp::ExitInstance() 
+{
+	// TODO: Add your specialized code here and/or call the base class
+	if (m_hLonDll != NULL)
+	{
+		FreeLibrary(m_hLonDll);
+		m_hLonDll = NULL;
+	}
+	return CWinApp::ExitInstance();
+}
